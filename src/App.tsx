a6fb8a2f6b2a9e5365e734c87105effe70174875
tsune <tsune@aluco.jp>
2025-05-16 12:03:02 +0900

    [Add] template

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-blue-500">
+        <h1 className="text-white text-4xl font-bold">Tailwind CSS 動作確認</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
